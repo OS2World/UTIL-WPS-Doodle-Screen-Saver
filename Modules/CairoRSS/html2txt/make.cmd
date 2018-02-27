196c2775a0b9e168d4d0703255662601ff977026
Dave Yeo <dave.r.yeo@gmail.com>
2018-02-26 21:12:41 -0800

    Compilation fixes Target i686 instead of original Pentium+MMX and remove an EMX leftover (-Zmt)

--- Modules/CairoRSS/html2txt/make.cmd
+++ Modules/CairoRSS/html2txt/make.cmd
@@ -1,12 +1,12 @@
-g++ -c -O2 -Zomf -Zmt -march=pentium -mmmx -DAUTO_PTR_BROKEN html2text.c
-g++ -c -O2 -Zomf -Zmt -march=pentium -mmmx -DAUTO_PTR_BROKEN html.c
-g++ -c -O2 -Zomf -Zmt -march=pentium -mmmx -DAUTO_PTR_BROKEN HTMLControl.c
-g++ -c -O2 -Zomf -Zmt -march=pentium -mmmx -DAUTO_PTR_BROKEN HTMLParser.c
-g++ -c -O2 -Zomf -Zmt -march=pentium -mmmx -DAUTO_PTR_BROKEN Area.c
-g++ -c -O2 -Zomf -Zmt -march=pentium -mmmx -DAUTO_PTR_BROKEN format.c
-g++ -c -O2 -Zomf -Zmt -march=pentium -mmmx -DAUTO_PTR_BROKEN sgml.c
-g++ -c -O2 -Zomf -Zmt -march=pentium -mmmx -DAUTO_PTR_BROKEN table.c
-g++ -c -O2 -Zomf -Zmt -march=pentium -mmmx -DAUTO_PTR_BROKEN Properties.c
-g++ -c -O2 -Zomf -Zmt -march=pentium -mmmx -DAUTO_PTR_BROKEN cmp_nocase.c
+g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN html2text.c
+g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN html.c
+g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN HTMLControl.c
+g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN HTMLParser.c
+g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN Area.c
+g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN format.c
+g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN sgml.c
+g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN table.c
+g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN Properties.c
+g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN cmp_nocase.c
 
-g++ -Zdll -Zomf -Zmt -Zmap html2txt.def html2text.o html.o HTMLControl.o HTMLParser.o Area.o format.o sgml.o table.o Properties.o cmp_nocase.o -o html2txt.dll
+g++ -Zdll -Zomf -Zmap html2txt.def html2text.o html.o HTMLControl.o HTMLParser.o Area.o format.o sgml.o table.o Properties.o cmp_nocase.o -o html2txt.dll
