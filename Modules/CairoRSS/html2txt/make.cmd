855444bafe8596409e6ea5622f6bf15fb724e8e1
Dave Yeo <dave.r.yeo@gmail.com>
2019-07-26 20:50:03 -0700

    Make sure the DLL is rebuilt

--- Modules/CairoRSS/html2txt/make.cmd
+++ Modules/CairoRSS/html2txt/make.cmd
@@ -9,4 +9,6 @@ g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN table.c
 g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN Properties.c
 g++ -c -O2 -Zomf -march=i686 -DAUTO_PTR_BROKEN cmp_nocase.c
 
+del html2txt.dll
+
 g++ -Zdll -Zomf -Zmap html2txt.def html2text.o html.o HTMLControl.o HTMLParser.o Area.o format.o sgml.o table.o Properties.o cmp_nocase.o -o html2txt.dll
