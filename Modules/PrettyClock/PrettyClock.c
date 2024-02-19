702278c27cb2ab08615f2dbc5b9cb9cb1d3c93f6
Dave Yeo <dave.r.yeo@gmail.com>
2024-02-19 08:11:57 -0800

    Stop calling a function twice

--- Modules/PrettyClock/PrettyClock.c
+++ Modules/PrettyClock/PrettyClock.c
@@ -873,6 +873,8 @@ void internal_LoadImage(char *pchHomeDirectory, char *pchImageName, Image_p pIma
     // Update info_ptr
     png_read_update_info(png_ptr, info_ptr);
 
+#if 0 //To quiet stderr from outputting "libpng warning: Ignoring extra png_read_update_info() call; row buffer not reallocated"
+
     // Get width, height and other stuffs again
     png_get_IHDR(png_ptr, info_ptr, &ulWidth, &ulHeight, &iBitDepth, &iColorType, NULL, NULL, NULL);
 
@@ -883,6 +885,7 @@ void internal_LoadImage(char *pchHomeDirectory, char *pchImageName, Image_p pIma
     // Update info_ptr
     png_read_update_info(png_ptr, info_ptr);
 
+#endif 
 
     // Get new (final) width, height etc..
     png_get_IHDR(png_ptr, info_ptr, &ulWidth, &ulHeight, &iBitDepth, &iColorType, NULL, NULL, NULL);
