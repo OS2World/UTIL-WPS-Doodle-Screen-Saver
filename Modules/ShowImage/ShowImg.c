98c3028e780ff1b0ac6c5ed8d2202181fafa74c9
Dave Yeo <dave.r.yeo@gmail.com>
2024-02-19 20:18:08 -0800

    More fixed version strings

--- Modules/ShowImage/ShowImg.c
+++ Modules/ShowImage/ShowImg.c
@@ -1027,8 +1027,8 @@ SSMODULEDECLSPEC int SSMODULECALL SSModule_GetModuleDesc(SSModuleDesc_p pModuleD
     return SSMODULE_ERROR_INVALIDPARAMETER;
 
   // Return info about module!
-  pModuleDesc->iVersionMajor = 1;
-  pModuleDesc->iVersionMinor = 81;
+  pModuleDesc->iVersionMajor = 2;
+  pModuleDesc->iVersionMinor = 0;
   strcpy(pModuleDesc->achModuleName, "Show Image");
   strcpy(pModuleDesc->achModuleDesc,
          "Show an image and blank the screen after some time.\n"
