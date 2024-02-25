2397e864d8c6053f29e985f527146454be124272
Dave Yeo <dave.r.yeo@gmail.com>
2024-02-24 21:34:54 -0800

    Better cleaning

--- cleanall.cmd
+++ cleanall.cmd
@@ -6,15 +6,18 @@ echo * SSCORE.DLL
 wmake -h clean
 
 echo * WPSSDESK.DLL
-cd wpssdesktop
-del *.dll
-del *.obj
-cd ..
+cd WPSSDesktop
+if exist *.dll del *.dll
+if exist *.obj del *.obj
+cd Languages
+if exist *.msg del *.msg
+if exist *.hlp del *.hlp
+cd ..\..
 
 echo * SSModWrp
 cd ssmodwrp
-del *.exe
-del *.obj
+if exist *.exe del *.exe
+if exist *.obj del *.obj
 cd ..
 
 echo * TESTER
@@ -30,81 +33,113 @@ cd ..
 echo * MODULES\ANTVSIM
 cd Modules\AnTVSim
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\AOS_Logo
 cd Modules\AOS_Logo
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\BLANK
 cd Modules\Blank
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\CairoClock
 cd Modules\CairoClock
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\CairoShapes
 cd Modules\CairoShapes
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\CIRCLES
 cd Modules\Circles
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\ECSBALL
 cd Modules\eCSBall
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\IFSIM
 cd Modules\IFSIM
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\MATRIX
 cd Modules\Matrix
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\PADLOCK
 cd Modules\Padlock
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\PRETTYCLOCK
 cd Modules\PrettyClock
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\RANDOMIZER
 cd Modules\Randomizer
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\SNOW
 cd Modules\Snow
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\SHOWIMAGE
 cd Modules\ShowImage
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\TEXT
 cd Modules\Text
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * MODULES\SLIDESHOW
 cd Modules\SlideShow
 wmake -h clean
-cd ..\..
+cd Languages
+del *.msg
+cd ..\..\..
 
 echo * All cleaned!
